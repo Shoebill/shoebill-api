c06e8216d629637f91a9862e56a6107643d6509f
mk124 <mk124a@gmail.com>
2012-09-30 06:18:46 +0800

    Add mixed interfaces.
    Signed-off-by: mk124 <mk124a@gmail.com>

---
+++ src/main/java/net/gtaun/shoebill/object/PlayerLabel.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (C) 2011 MK124
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.gtaun.shoebill.object;
+
+import net.gtaun.shoebill.object.primitive.PlayerLabelPrim;
+import net.gtaun.shoebill.proxy.ProxyManager;
+import net.gtaun.shoebill.trait.TraitManager;
+
+
+/**
+ * @author MK124
+ *
+ */
+
+public interface PlayerLabel extends PlayerLabelPrim, ProxyManager, TraitManager
+{
+
+}
