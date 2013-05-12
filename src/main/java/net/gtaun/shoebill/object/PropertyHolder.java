085f4f63609e802bb9b7726ac5986cbc75dead24
mk124 <mk124a@gmail.com>
2013-05-13 05:45:50 +0800

    Add PropertyHolder interface.
    Signed-off-by: mk124 <mk124a@gmail.com>

---
+++ src/main/java/net/gtaun/shoebill/object/PropertyHolder.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (C) 2013 MK124
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.gtaun.shoebill.object;
+
+/**
+ * 
+ * 
+ * @author MK124
+ */
+public interface PropertyHolder
+{
+	Object getProperty(Object key);
+	<T> T getProperty(Class<T> clz);
+	
+	void setProperty(Object key, Object value);
+	<T> void setProperty(Class<T> clz, T value);
+
+	void setWeakProperty(Object key, Object value);
+	<T> void setWeakProperty(Class<T> clz, T value);
+	
+	boolean isPropertyExisted(Object key);
+	boolean removeProperty(Object key);
+}
