4bf1c72bca3216bd73cca8fc07660f99a55a1764
Marvin H <catboy560@gmail.com>
2014-11-01 19:06:55 +0100

    Changes for VehicleLifecycleHolder

--- src/main/java/net/gtaun/shoebill/event/vehicle/VehicleCreateEvent.java
+++ src/main/java/net/gtaun/shoebill/event/vehicle/VehicleCreateEvent.java
@@ -1,7 +1,12 @@
 package net.gtaun.shoebill.event.vehicle;
 
+import net.gtaun.shoebill.object.Vehicle;
+
 /**
  * Created by marvin on 01.11.14.
  */
-public class VehicleCreateEvent {
+public class VehicleCreateEvent extends VehicleEvent {
+    public VehicleCreateEvent(Vehicle vehicle) {
+        super(vehicle);
+    }
 }
--- src/main/java/net/gtaun/shoebill/event/vehicle/VehicleDestroyEvent.java
+++ src/main/java/net/gtaun/shoebill/event/vehicle/VehicleDestroyEvent.java
@@ -1,7 +1,12 @@
 package net.gtaun.shoebill.event.vehicle;
 
+import net.gtaun.shoebill.object.Vehicle;
+
 /**
  * Created by marvin on 01.11.14.
  */
-public class VehicleDestroyEvent {
+public class VehicleDestroyEvent extends VehicleEvent {
+    public VehicleDestroyEvent(Vehicle vehicle) {
+        super(vehicle);
+    }
 }
